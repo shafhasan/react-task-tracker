f53e601d2b32c75e10775f89150a310f8d2a151e
Shafqat Hasan <60235150+shafhasan@users.noreply.github.com>
2021-04-21 04:15:00 +0600

    useEffect and Fetching from server

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appoinment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-  ])
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   // Add Task
   const addTask = (task) => {
@@ -49,8 +47,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {/* If there are no tasks, show this message */}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,21 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title }) => {
-const onClick = () => {
-  console.log('click')
-}
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className = 'header' >
+    <header className='header' >
       <h1>{title}</h1>
-      <Button color = 'green' text = 'Add' onClick = {onClick} />
+      <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 }
 
 Header.defaultProps = {
-  title : 'Task Tracker'
+  title: 'Task Tracker'
 }
 
 Header.propTypes = {
-  title : PropTypes.string.isRequired
+  title: PropTypes.string.isRequired
 }
 
 // CSS in JS
